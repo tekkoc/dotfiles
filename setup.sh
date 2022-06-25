ddf1d5b62eb1f31553b68843580acf0f03f3f46b
tekkoc <tekkoc@gmail.com>
2022-06-25 23:20:21 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -27,7 +27,9 @@ ln -sf `pwd`/tmux.conf ~/.tmux.conf
 ln -sf `pwd`/gitconfig ~/.gitconfig
 ln -sf `pwd`/gitignore_global ~/.gitignore_global
 
-ln -sf `pwd`/karabiner-elements.json ~/.config/karabiner/assets/complex_modifications/1984.json
+if [ ! -e ~/.config/karabiner ] ; then
+  ln -sf `pwd`/karabiner-elements.json ~/.config/karabiner/assets/complex_modifications/1984.json
+fi
 
 ln -sf `pwd`/hammerspoon.lua ~/.hammerspoon/init.lua
 
