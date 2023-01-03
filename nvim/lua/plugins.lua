ed2c566427e57dab915cadf29cd1cf1146d3591d
tekkoc <tekkoc@gmail.com>
2023-01-03 22:21:39 +0900

    add lualine

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -82,7 +82,38 @@ return require('packer').startup(function(use)
     end,
   }
 
-  -- status line
+  use {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup {
+        options = {
+          icons_enabled = false,
+          theme = 'auto',
+          component_separators = { left = '', right = ''},
+          section_separators = { left = '', right = ''},
+          always_divide_middle = true,
+          globalstatus = false,
+        },
+        sections = {
+          lualine_a = {},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {{'filename', path = 1}},
+          lualine_x = {'filetype'},
+          lualine_y = {},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {'diff'},
+          lualine_b = {{'filename', path = 1}},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {'location'},
+        },
+      }
+    end,
+  }
+
   -- use 'dense-analysis/ale'
   -- nvcode-color-scheme
   -- filer
