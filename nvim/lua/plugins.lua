877812c23c6a7d0b83d08e70286178ac88ee1b27
tekkoc <tekkoc@gmail.com>
2023-01-05 14:59:39 +0900

    formatを確実に行うように

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -216,25 +216,19 @@ return require("packer").startup(function(use)
 	use({
 		"jose-elias-alvarez/null-ls.nvim",
 		config = function()
-			local null_ls = require("null-ls")
+			vim.api.nvim_create_augroup("lsp_format", {})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = "lsp_format",
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
 
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+			local null_ls = require("null-ls")
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
 				},
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({ bufnr = bufnr })
-							end,
-						})
-					end
-				end,
 			})
 		end,
 	})
