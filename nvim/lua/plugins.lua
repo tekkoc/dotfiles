ef75c0627b2330cc5b0aedfc9b84ad28fc88f0d5
tekkoc <tekkoc@gmail.com>
2023-01-05 14:44:04 +0900

    add null-ls

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -213,6 +213,18 @@ return require('packer').startup(function(use)
   }
 
   use {
+    'jose-elias-alvarez/null-ls.nvim',
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+          sources = {
+            null_ls.builtins.formatting.stylua,
+          },
+        })
+    end,
+  }
+
+  use {
     "hrsh7th/nvim-cmp",
     requires = {
       "hrsh7th/cmp-nvim-lsp",
