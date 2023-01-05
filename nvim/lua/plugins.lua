70b5cd2c99493061c6a282f91490ba920bdebdfd
tekkoc <tekkoc@gmail.com>
2023-01-05 14:49:40 +0900

    保存時にフォーマットするように

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -216,10 +216,24 @@ return require('packer').startup(function(use)
     'jose-elias-alvarez/null-ls.nvim',
     config = function()
       local null_ls = require("null-ls")
+
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
       null_ls.setup({
           sources = {
             null_ls.builtins.formatting.stylua,
           },
+          on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+              vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                  group = augroup,
+                  buffer = bufnr,
+                  callback = function()
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                  end,
+                })
+            end
+          end,
         })
     end,
   }
